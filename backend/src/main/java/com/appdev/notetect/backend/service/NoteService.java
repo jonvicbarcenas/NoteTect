29190c1543611b753946e3c807afb140ae5827a6
LAB24_USER ON WS42 <L24X12W42@LabHCI.net>
2025-11-21 11:01:04 +0800

    make service classess

---
+++ backend/src/main/java/com/appdev/notetect/backend/service/NoteService.java
@@ -0,0 +1,31 @@
+package com.appdev.notetect.backend.service;
+
+import com.appdev.notetect.backend.entity.Note;
+import com.appdev.notetect.backend.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NoteService {
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    public Optional<Note> getNoteById(int id) {
+        return noteRepository.findById(id);
+    }
+
+    public Note saveNote(Note note) {
+        return noteRepository.save(note);
+    }
+
+    public void deleteNote(int id) {
+        noteRepository.deleteById(id);
+    }
+}
